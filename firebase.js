0d56d3f4cfa3d18c35450f53748f4c8c1e7c2ac6
yagizkarakus <yagizhikmetkarakus@gmail.com>
2023-03-21 18:25:06 +0300

    login commit

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,15 +18,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-    const app = firebase.initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-}
-else {
-    app = firebase.app()
-}
 
-const auth = firebase.auth()
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export {auth};  
